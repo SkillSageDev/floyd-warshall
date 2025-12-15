57b47fbb9daa45cf592dfe9fe57f8de3b6a0342f
SkillSageDev <147807207+SkillSageDev@users.noreply.github.com>
2025-12-15 11:51:05 +0200

    refactor(graphToMatrix): remove sort part
    Delete node variable which takes the graph.keys() and sort it to
    generate keyIndex instead passed graph.keys() immediatly without sorting
    it.
    
    With sorting wrong values will be assigned to each key in the for loop
    if the graph not already sorted according to the keys.

--- floyd-warshall.py
+++ floyd-warshall.py
@@ -1,39 +1,40 @@
-from math import inf
 import numpy as np
 
 
-def floydWarshall (graph):
+def floydWarshall(graph):
     a = graphToMatrix(graph)
-    size =a.shape[0] #size of single row or single column
-    for k in range(0,size):
-        for i in range(0,size):
-            for j in range(0,size):
-                if i == j or i==k or j==k:
+    size = a.shape[0]  # size of single row or single column
+    for k in range(0, size):
+        for i in range(0, size):
+            for j in range(0, size):
+                if i == j or i == k or j == k:
                     continue
                 else:
-                    a[i,j] = min(a[i,j],a[i,k]+a[k,j])
-    print (a)
+                    a[i, j] = min(a[i, j], a[i, k] + a[k, j])
+    print(a)
 
 
 def graphToMatrix(graph):
-    node = sorted(graph.keys())
-    keyIndex = {key: i for i, key in enumerate(node)} #this is a variable that makes a dictionary by making a loop; to say that ex: A -> 0, B -> 1, C -> 2 & so on
-    size = len(graph) #size of single row, or single column
-    a0 = np.full((size,size),np.inf)
+    keyIndex = {
+        key: i for i, key in enumerate(graph.keys())
+    }  # this is a variable that makes a dictionary by making a loop; to say that ex: A -> 0, B -> 1, C -> 2 & so on
+    size = len(graph)  # size of single row, or single column
+    a0 = np.full((size, size), 1000)
     for edge in graph:
-        b =graph[edge]
+        b = graph[edge]
         for directedEdge in b:
             i = keyIndex[edge]
             j = keyIndex[directedEdge]
             a0[i][j] = b[directedEdge]
         np.fill_diagonal(a0, 0)
-    return (a0)
+    return a0
+
 
 graph = {
-     'A': {'B':2,'D':3},
-     'B': {'A':3,'C':2},
-     'C': {'D':4},
-     'D': {'A':-2,'B':6}
+    "A": {"B": 2, "D": 3},
+    "B": {"A": 3, "C": 2},
+    "C": {"D": 4},
+    "D": {"A": -2, "B": 6},
 }
 
 floydWarshall(graph)
